a4a68205dd569eb17952e430dd6253189949580c
Chewie <chewbacca31150@gmail.com>
2018-03-08 15:26:51 +0100

    remove mongo

--- covoiturage-server/src/main/java/com/covoit/config/MongoEmbeddedConfig.java
+++ covoiturage-server/src/main/java/com/covoit/config/MongoEmbeddedConfig.java
@@ -1,16 +1,16 @@
 package com.covoit.config;
-
+/*
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
+import com.mongodb.MongoClient;*/
 
-@Configuration
+//@Configuration
 public class MongoEmbeddedConfig {
-    @Value("${mongodb.url:0}")
+    /*@Value("${mongodb.url:0}")
     private static final String MONGO_DB_URL = "localhost";
 
     @Value("${mongodb.name:0}")
@@ -24,5 +24,5 @@ public class MongoEmbeddedConfig {
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
         return new MongoTemplate(mongo(), MONGO_DB_NAME);
-    }
+    }*/
 }
