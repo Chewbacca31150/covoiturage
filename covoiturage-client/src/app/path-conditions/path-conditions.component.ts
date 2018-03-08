46450631df60bab701b99f9b250fd03ae8e9da86
K CORP <pierre.cassignol@ynov.com>
2018-03-08 14:56:40 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ covoiturage-client/src/app/path-conditions/path-conditions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-path-conditions',
+  templateUrl: './path-conditions.component.html',
+  styleUrls: ['./path-conditions.component.css']
+})
+export class PathConditionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
