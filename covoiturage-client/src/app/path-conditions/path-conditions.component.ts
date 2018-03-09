aa87f9a5d928c9488b985c3c6b5cab3d8b3c5c2e
K CORP <pierre.cassignol@ynov.com>
2018-03-09 17:15:32 +0100

    Reverse check on Path smoking condition for both (Driver+Passenger)

--- covoiturage-client/src/app/path-conditions/path-conditions.component.ts
+++ covoiturage-client/src/app/path-conditions/path-conditions.component.ts
@@ -15,7 +15,13 @@ export class PathConditionsComponent implements OnInit {
   thumbLabel: boolean
   value: number
   tickInterval:number = 1;
+  smoke:boolean = false;
+  noSmoke:boolean =false;
+  smokePassenger:boolean = false;
+  noSmokePassenger:boolean =false;
+
   constructor() { }
+
 
   ngOnInit() {
     this.autoTicks = true;
@@ -27,6 +33,25 @@ export class PathConditionsComponent implements OnInit {
     this.value = 0;
   }
 
+  reverseSmokeCheckbox(){
+    this.smoke = !this.smoke;
+    this.noSmoke = false;
+  }
+  
+  reverseNoSmokeCheckbox(){
+    this.noSmoke = !this.noSmoke;
+    this.smoke = false;
+  }
+
+  reverseSmokeCheckboxPassenger(){
+    this.smokePassenger = !this.smokePassenger;
+    this.noSmokePassenger = false;
+  }
+  
+  reverseNoSmokeCheckboxPassenger(){
+    this.noSmokePassenger = !this.noSmokePassenger;
+    this.smokePassenger = false;
+  }
 
   private _tickInterval = 1;
 
