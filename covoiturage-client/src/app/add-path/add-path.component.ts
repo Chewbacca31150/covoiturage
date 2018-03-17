ad73238f772900ca66088a6bf7f8ccb3676d3726
K CORP <pierre.cassignol@ynov.com>
2018-03-17 18:29:49 +0100

    add path Page + responsive Footer + minor corrections

--- covoiturage-client/src/app/add-path/add-path.component.ts
+++ covoiturage-client/src/app/add-path/add-path.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-path',
-  templateUrl: './add-path.component.html',
-  styleUrls: ['./add-path.component.css']
+    selector: 'app-add-path',
+    templateUrl: './add-path.component.html',
+    styleUrls: ['./add-path.component.css']
 })
 export class AddPathComponent implements OnInit {
 
-  constructor() { }
+    pathRegular: String
+    pathBack: Boolean
+    regularPath: string
 
-  ngOnInit() {
-  }
+    toppings = new FormControl();
+    toppingList = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    setradio(e: string): void {
+        this.regularPath = e;
+    }
+
+    isSelected(name: string): boolean {
+        if (!this.regularPath) // if no radio button is selected, always return false so every nothing is shown  
+        {
+            return false;
+        }
+        return (this.regularPath === name); // if current radio button is selected, return true, else return false  
+    }
 
 }
--- covoiturage-client/src/app/app.module-material.ts
+++ covoiturage-client/src/app/app.module-material.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule, MatTabsModule, MatSliderModule, MatCardModule, MatMenuModule, MatFormFieldModule, MatToolbarModule, MatTableModule, MatIconModule, MatCheckboxModule, MatAutocompleteModule, MatButtonToggleModule, MatInputModule, MatGridListModule, MatDialogModule, MatSnackBarModule, MatSelectModule } from '@angular/material';
+import { MatButtonModule, MatRadioModule, MatDatepickerModule, MatNativeDateModule, MatTabsModule, MatSliderModule, MatCardModule, MatMenuModule, MatFormFieldModule, MatToolbarModule, MatTableModule, MatIconModule, MatCheckboxModule, MatAutocompleteModule, MatButtonToggleModule, MatInputModule, MatGridListModule, MatDialogModule, MatSnackBarModule, MatSelectModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatTabsModule, MatSliderModule, MatCardModule, MatMenuModule, MatFormFieldModule, MatToolbarModule, MatTableModule, MatIconModule, MatCheckboxModule, MatAutocompleteModule, MatButtonToggleModule, MatInputModule, MatGridListModule, MatDialogModule, MatSnackBarModule, MatSelectModule],
-  exports: [MatButtonModule, MatTabsModule, MatSliderModule, MatCardModule, MatMenuModule, MatFormFieldModule, MatToolbarModule, MatTableModule, MatIconModule, MatCheckboxModule, MatAutocompleteModule, MatButtonToggleModule, MatInputModule, MatGridListModule, MatDialogModule, MatSnackBarModule, MatSelectModule],
+  imports: [MatButtonModule, MatRadioModule, MatNativeDateModule, MatDatepickerModule, MatTabsModule, MatSliderModule, MatCardModule, MatMenuModule, MatFormFieldModule, MatToolbarModule, MatTableModule, MatIconModule, MatCheckboxModule, MatAutocompleteModule, MatButtonToggleModule, MatInputModule, MatGridListModule, MatDialogModule, MatSnackBarModule, MatSelectModule],
+  exports: [MatButtonModule, MatRadioModule, MatNativeDateModule, MatDatepickerModule, MatTabsModule, MatSliderModule, MatCardModule, MatMenuModule, MatFormFieldModule, MatToolbarModule, MatTableModule, MatIconModule, MatCheckboxModule, MatAutocompleteModule, MatButtonToggleModule, MatInputModule, MatGridListModule, MatDialogModule, MatSnackBarModule, MatSelectModule],
 })
 export class AppModuleMaterial { }
