08f9cfc5d7037f192a70ee09f8e7c822ab1ae3f5
Chewie <chewbacca31150@gmail.com>
2018-04-06 15:41:06 +0200

    EIN KOMITEUH

--- covoiturage-client/src/app/add-path/add-path.component.ts
+++ covoiturage-client/src/app/add-path/add-path.component.ts
@@ -11,141 +11,126 @@ import { MatSnackBar } from '@angular/material';
 import { RegularDays } from '../models/regulardays';
 
 @Component({
-  selector: 'app-add-path',
-  templateUrl: './add-path.component.html',
-  styleUrls: ['./add-path.component.css']
+    selector: 'app-add-path',
+    templateUrl: './add-path.component.html',
+    styleUrls: ['./add-path.component.css']
 })
 export class AddPathComponent implements OnInit {
 
-  hourList = ['6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30'];
-  pathRegular: string;
-  pathBack: boolean;
-  regularPath: string;
-  form: FormGroup;
-  toppingList = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+    hourList = ['6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30'];
+    pathRegular: string;
+    pathBack: boolean;
+    regularPath: string;
+    form: FormGroup;
+    toppingList = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
 
-  @ViewChild('startAddress')
-  public startAddress: ElementRef;
-  @ViewChild('stopAddress')
-  public stopAddress: ElementRef;
+    @ViewChild('startAddress')
+    public startAddress: ElementRef;
+    @ViewChild('stopAddress')
+    public stopAddress: ElementRef;
 
-  startLocation: LocationGoogle = {
-    lat: null,
-    lng: null,
-    address: null
-  };
-  stopLocation: LocationGoogle = {
-    lat: null,
-    lng: null,
-    address: null
-  };
+    startLocation: LocationGoogle = {
+        lat: null,
+        lng: null,
+        address: null
+    };
+    stopLocation: LocationGoogle = {
+        lat: null,
+        lng: null,
+        address: null
+    };
 
-  constructor(private trajetService: TrajetService, private authService: AuthService,
-              private formBuilder: FormBuilder, private mapsAPILoader: MapsAPILoader, private route: Router, private snackBar: MatSnackBar) {
-  }
+    constructor(private trajetService: TrajetService, private authService: AuthService,
+        private formBuilder: FormBuilder, private mapsAPILoader: MapsAPILoader, private route: Router, private snackBar: MatSnackBar) {
+    }
 
-  ngOnInit() {
-    this.form = this.formBuilder.group({
-      startAddress: [''],
-      stopAddress: [''],
-      numberPlaces: [''],
-      pathDepartureDate: [''],
-      pathDepartureHour: [''],
-      pathRegularDays: [''],
-      pathBackFormControl: ['']
-    });
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            startAddress: [''],
+            stopAddress: [''],
+            numberPlaces: [''],
+            pathDepartureDate: [''],
+            pathDepartureHour: [''],
+            pathRegularDays: [''],
+            pathBackFormControl: ['']
+        });
 
-    this.mapsAPILoader.load().then(() => {
-      const startAddressAutocomplete = new google.maps.places.Autocomplete(this.startAddress.nativeElement, {
-        types: ['address']
-      });
+        this.mapsAPILoader.load().then(() => {
+            const startAddressAutocomplete = new google.maps.places.Autocomplete(this.startAddress.nativeElement, {
+                types: ['address']
+            });
 
-      startAddressAutocomplete.addListener('place_changed', () => {
-        const place: google.maps.places.PlaceResult = startAddressAutocomplete.getPlace();
+            startAddressAutocomplete.addListener('place_changed', () => {
+                const place: google.maps.places.PlaceResult = startAddressAutocomplete.getPlace();
 
-        // verify result
-        if (place.geometry === undefined || place.geometry === null) {
-          return;
+                // verify result
+                if (place.geometry === undefined || place.geometry === null) {
+                    return;
+                }
+
+                this.startLocation = {
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng(),
+                    address: place.formatted_address
+                };
+            });
+
+            const stopAddressAutocomplete = new google.maps.places.Autocomplete(this.stopAddress.nativeElement, {
+                types: ['address']
+            });
+            stopAddressAutocomplete.addListener('place_changed', () => {
+                const place: google.maps.places.PlaceResult = stopAddressAutocomplete.getPlace();
+
+                // verify result
+                if (place.geometry === undefined || place.geometry === null) {
+                    return;
+                }
+
+                this.stopLocation = {
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng(),
+                    address: place.formatted_address
+                };
+            });
+        })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    setradio(e: string): void {
+        this.regularPath = e;
+    }
+
+    isSelected(name: string): boolean {
+        // if no radio button is selected, always return false so every nothing is shown
+        if (!this.regularPath) {
+            return false;
         }
+        return (this.regularPath === name); // if current radio button is selected, return true, else return false
+    }
 
-        this.startLocation = {
-          lat: place.geometry.location.lat(),
-          lng: place.geometry.location.lng(),
-          address: place.formatted_address
-        };
-      });
-
-      const stopAddressAutocomplete = new google.maps.places.Autocomplete(this.stopAddress.nativeElement, {
-        types: ['address']
-      });
-      stopAddressAutocomplete.addListener('place_changed', () => {
-        const place: google.maps.places.PlaceResult = stopAddressAutocomplete.getPlace();
-
-        // verify result
-        if (place.geometry === undefined || place.geometry === null) {
-          return;
+    onSubmit(event: Event) {
+        if (this.form.value.startAddress === '' || this.form.value.stopAddress === '' ||
+            this.form.value.numberPlaces === '' || (this.form.value.pathDepartureDate === ''
+                && this.form.value.pathRegularDays === '')) {
+            return;
         }
-
-        this.stopLocation = {
-          lat: place.geometry.location.lat(),
-          lng: place.geometry.location.lng(),
-          address: place.formatted_address
-        };
-      });
-    })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  setradio(e: string): void {
-    this.regularPath = e;
-  }
-
-  isSelected(name: string): boolean {
-    // if no radio button is selected, always return false so every nothing is shown
-    if (!this.regularPath) {
-      return false;
-    }
-    return (this.regularPath === name); // if current radio button is selected, return true, else return false
-  }
-
-  onSubmit(event: Event) {
-    if (this.form.value.startAddress === '' || this.form.value.stopAddress === '' ||
-      this.form.value.numberPlaces === '' || (this.form.value.pathDepartureDate === ''
-        && this.form.value.pathRegularDays === '')) {
-      return;
-    }
-    event.preventDefault();
-    if (this.regularPath === 'pathRegularTrue') {
-      this.form.controls['pathDepartureDate'].setValue('');
-    } else if (this.regularPath === 'pathRegularFalse') {
-      this.form.controls['pathRegularDays'].setValue('');
-    }
-
-    const form = this.form.value;
-    let pathRegularDays: RegularDays[] = [];
-    if (form.pathRegularDays === '') {
-      pathRegularDays = null;
-    } else {
-      const arry: String[] = form.pathRegularDays;
-      arry.forEach(str => {
-        pathRegularDays.push({message: str});
-      });
-      console.log(pathRegularDays);
-    }
-
+        event.preventDefault();
+        if (this.regularPath === 'pathRegularTrue') {
+            this.form.controls['pathDepartureDate'].setValue('');
+        } else if (this.regularPath === 'pathRegularFalse') {
+            this.form.controls['pathRegularDays'].setValue('');
+        }
 
         const form = this.form.value;
-        let pathRegularDays;
+        let pathRegularDays: String;
         if (form.pathRegularDays === '') {
             pathRegularDays = null;
         } else {
             const arry: String[] = form.pathRegularDays;
             pathRegularDays = arry.join(',');
-
         }
-
 
         const trajet: Trajet = {
             dateDeparture: form.pathDepartureDate,
@@ -162,7 +147,7 @@ export class AddPathComponent implements OnInit {
             stopLocation: this.stopLocation
         };
         this.trajetService.saveTrajet(trajet).subscribe((a) => console.log(a));
-        this.snackBar.open("Trajet ajoute.","", {
+        this.snackBar.open('Trajet ajoute.', '', {
             duration: 3500,
             horizontalPosition: 'right',
             verticalPosition: 'top'
